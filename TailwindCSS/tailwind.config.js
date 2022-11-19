4cdddf52a784e215854c8929a22536a020933a0f
Hooman734 <hesamyan@outlook.com>
2022-11-19 19:03:25 +0400

    got tailwind.config updateed

--- TailwindCSS/tailwind.config.js
+++ TailwindCSS/tailwind.config.js
@@ -1,11 +1,8 @@
 
 module.exports = {
-    purge: [],
+    content: [],
     darkMode: false,
     theme: {
-        extend: {},
-    },
-    variants: {
         extend: {},
     },
     plugins: [],
